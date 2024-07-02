5aed314ffe0a791aa757f2a83807ce9d2bcbf4ab
yoonwooiOS <yoonwoo4429@kakao.com>
2024-07-02 16:36:21 +0900

    Initial Commit

---
+++ Reminder/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Reminder
+//
+//  Created by 김윤우 on 7/2/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
